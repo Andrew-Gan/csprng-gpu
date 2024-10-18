e1221149ca4cacdd9f3a42f01fb4b438e7829e6b
Andrew Gan <andrewgan120410@gmail.com>
2024-10-17 20:27:02 -0400

    add opacus

--- dockerfile
+++ dockerfile
@@ -2,14 +2,17 @@ FROM nvidia/cuda:12.1.1-devel-ubuntu22.04
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt update && apt install -y bash g++ python3 python3-pip
-RUN pip3 install torch
+RUN pip3 install torch torchvision numpy
 ENV TORCH_CUDA_ARCH_LIST=8.6
 ENV FORCE_CUDA=1
 
 COPY ./csprng /home/csprng
 WORKDIR /home/csprng
-
 RUN python3 setup.py install
+
+COPY ./opacus /home/opacus
+WORKDIR /home/opacus
+RUN pip install -e .
 
 COPY test.py /home
 WORKDIR /home
--- test.py
+++ test.py
@@ -3,12 +3,15 @@ import torch
 import torchcsprng as csprng
 import time
 
+from torch import nn, optim
+from torch.nn.functional import *
+from torchvision import datasets, transforms
+from opacus import PrivacyEngine
+
 def test_aes(device: str):
     key = torch.empty(16, dtype=torch.uint8, device=device).random_(0, 256)
 
     for i in range(19, 24):
-        if i > 19:
-            print('2^' + str(i) + ' blocks')
         size_bytes = (1 << i) * 16
         initial = torch.empty(size_bytes // 4, dtype=torch.float32, device=device).normal_(-24.0, 42.0)
 
@@ -25,26 +28,65 @@ def test_aes(device: str):
         assert(decrypted == initial).all()
 
         if i > 19:
-            print("%s %.2f ms" % (dev, (end - start) * 1000))
+            print(f'2^{i}: {dev} {(end-start)*1000:.2f} ms')
 
-def test_prng(device: str):
+def test_csprng(device: str):
     urandom_gen = csprng.create_random_device_generator('/dev/urandom')
-    for i in range(20, 25):
+    for i in range(19, 24):
         start = time.time()
-        torch.randn(1 << i, device='cpu', generator=urandom_gen)
-        mid = time.time()
-        torch.randn(1 << i, device='cuda', generator=urandom_gen)
+        torch.randn(1 << i, device=device, generator=urandom_gen)
         end = time.time()
 
-        if i > 20:
-            print("cpu %.2f ms" % ((mid - start) * 1000))
-            print("cuda %.2f ms" % ((end - mid) * 1000))
+        if i > 19:
+            print(f'2^{i}: {dev} {(end-start)*1000:.2f} ms')
+
+def test_dp(device: str):
+    class Net(nn.Module):      
+        def __init__(self):
+            super(Net, self).__init__()
+            self.conv1 = nn.Conv2d(1, 6, 5)
+            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.fc1 = nn.Linear(16*5*5, 120)
+            self.fc2 = nn.Linear(120, 10)
+            
+        def forward(self, x):
+            x = max_pool2d(relu(self.conv1(x)), (2,2))
+            x = max_pool2d(relu(self.conv2(x)), 2)    
+            x = x.view(x.size(0), -1)
+            x = relu(self.fc1(x))
+            x = self.fc2(x)
+            
+            return x
+    
+    dataset = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=transforms.ToTensor()
+    )
+
+    model = Net()
+    optimizer = optim.SGD(model.parameters(), lr=0.05)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=1024)
+
+    privacy_engine = PrivacyEngine()
+    model, optimizer, data_loader = privacy_engine.make_private(
+        module=model,
+        optimizer=optimizer,
+        data_loader=data_loader,
+        noise_multiplier=1.1,
+        max_grad_norm=1.0,
+    )
 
 if __name__ == '__main__':
     if torch.cuda.is_available():
-        print('Current CUDA device', torch.cuda.current_device())
+        print(f'Current CUDA device {torch.cuda.current_device()}\n')
     else:
         sys.exit('No CUDA device found')
-    for dev in ['cpu', 'cuda']:
-        print('using', dev)
-        test_aes(dev)
+
+    fname = ['aes', 'csprng', 'dp']
+    for name, test_func in zip(fname, [test_aes, test_csprng, test_dp]):
+        print(f'Testing {name}')
+        for dev in ['cpu', 'cuda']:
+            print(f'running on {dev}')
+            test_func(dev)
